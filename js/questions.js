b636c96ed35a5d726654c7660b2d49c1066c5e8a
Muhammadjon-is <isakovw18@gmail.com>
2022-10-19 17:21:20 +0200

    Epicode benchmark test almost done

---
+++ js/questions.js
@@ -0,0 +1,89 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language",
+    ],
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor",
+    ],
+  },
+  {
+    numb: 4,
+    question: "Which of the following is not an HTML tag?",
+    answer: "Doctype",
+    options: ["P", " Table", "Doctype", "Style"],
+  },
+  {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language",
+    ],
+  },
+
+  {
+    numb: 6,
+    question: "How many ways can you apply colors in CSS?",
+    answer: "3",
+    options: ["3", "2", "5", "4"],
+  },
+
+  {
+    numb: 7,
+    question: "What should be the very last thing in an HTML document? ",
+    answer: "Doc type",
+    options: ["The heading", "Title", "Body", "Doc type"],
+  },
+
+  {
+    numb: 8,
+    question:
+      "A webpage displays a picture. What tag was used to display that pictures ",
+    answer: "img",
+    options: ["img", "image", "src", "picture"],
+  },
+
+  {
+    numb:9,
+    question: "Which tag inserts a line horizontally on your web page?",
+    answer: "<hr>",
+    options: ["<line>", "<hr>", "<tr>", "<td>"],
+  },
+
+  {
+    numb:10,
+    question:"How can you make a bulleted list? ",
+    answer:"<ol>",
+    options: ["<ul>", "<list>", "<ol>", "<td>"],
+  },
+];
